e157f62bb3a636519f29643a03d3ba373d77a058
level <1926216041@qq.com>
2023-03-24 16:46:14 +0800

    leetCode 第200题

--- LeetCode/200.岛屿数量.java
+++ LeetCode/200.岛屿数量.java
@@ -5,12 +5,43 @@
  */
 
 // @lc code=start
-class Solution {
+class Solutiona {
+    int count = 0;
+
+    public static void main(String[] args) {
+        char[][] grid = { { '1', '1', '1', '1', 0 }, { '1', '1', '0', '1', '0' }, { '1', '1', '0', '0', '0' },
+                { '0', '0', '0', '0', '0' } };
+        Solutiona solution = new Solutiona();
+        int ts = solution.numIslands(grid);
+        System.out.println(ts);
+    }
+
     public int numIslands(char[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
 
-        return 0;
+                if (grid[i][j] == '1') {
+                    count = count + 1;
+                    printIsland(grid, i, j);
+                }
 
+            }
+        }
+        return count;
+
+    }
+
+    public void printIsland(char[][] grid, int i, int j) {
+        if (i >= 0 && i < grid.length && j >= 0 && j < grid[i].length && grid[i][j] == '1') {
+            grid[i][j] = '2';
+            printIsland(grid, i + 1, j);
+            printIsland(grid, i, j + 1);
+            printIsland(grid, i - 1, j);
+            printIsland(grid, i, j - 1);
+
+        } else {
+
+        }
     }
 }
 // @lc code=end
-
--- TestCode/test1.java
+++ TestCode/test1.java
@@ -2,6 +2,8 @@ package TestCode;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("你的话VCD是");    
+
+        System.out.println("你的话VCD是");
     }
 }
+    
